8356f7e2a7cfdb748c47dc21df42aa61fc5731a7
AlexanderSierraPulido <40802531+AlexanderSierraPulido@users.noreply.github.com>
2019-04-04 17:56:30 -0500

    Update graficas.py

--- graficas.py
+++ graficas.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-os.system("g++ Metropolis.cpp -o Metropolis.x")
-os.system("./Metropolis.x > datos.dat")
+os.system("g++ Ejercicio_clase22.cpp")
+os.system("./a.out")
 
 data = np.loadtxt("datos.dat")
 
